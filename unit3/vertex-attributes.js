4e689b177754221f531cdcb9eff0790404aa1d09
HanByul Yang <hanbyul.yang@gmail.com>
2023-04-12 14:27:13 +0900

    Vertex Attributes

--- unit3/vertex-attributes.js
+++ unit3/vertex-attributes.js
@@ -14,7 +14,16 @@ function fillScene() {
 	geometry = new THREE.Geometry();
 
 	// Student: add a colored triangle here
+	geometry.vertices.push( new THREE.Vector3( 100, 0, 0) );
+	geometry.vertices.push( new THREE.Vector3( 0, 100, 0) );
+	geometry.vertices.push( new THREE.Vector3( 0, 0, 100) );
 
+	geometry.faces.push( new THREE.Face3( 0, 1, 2 ) );
+	var color1 = new THREE.Color( 0xff0000 );
+	var color2 = new THREE.Color( 0x00ff00 );
+	var color3 = new THREE.Color( 0x0000ff );
+
+	geometry.faces[0].vertexColors = [ color1, color2, color3 ];
 
 	mesh = new THREE.Mesh( geometry, material );
 
