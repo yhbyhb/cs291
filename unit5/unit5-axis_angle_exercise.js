d3f2ba9424cb79070390d5cdac2bf142da341506
HanByul Yang <hanbyul.yang@gmail.com>
2023-06-02 13:31:21 +0900

    Make an Ornament or Caltrop

--- unit5/unit5-axis_angle_exercise.js
+++ unit5/unit5-axis_angle_exercise.js
@@ -47,16 +47,20 @@ function fillScene() {
 	// or just simply theta = Math.acos( cylAxis.y );
 
 	// YOUR CODE HERE
-	var cylinder = new THREE.Mesh(
-		new THREE.CylinderGeometry( 0.2, 0.2, cylLength, 32 ), cylinderMaterial );
-	var rotationAxis = new THREE.Vector3(1,0,-1);
-	// makeRotationAxis wants its axis normalized
-	rotationAxis.normalize();
-	// don't use position, rotation, scale
-	cylinder.matrixAutoUpdate = false;
-	cylinder.matrix.makeRotationAxis( rotationAxis, theta );
-	scene.add( cylinder );
-
+	var cylinderGeo = new THREE.CylinderGeometry( 0.2, 0.2, cylLength, 32 );
+	for (var i = 0; i < 4; i++) {
+		var cylinder = new THREE.Mesh(cylinderGeo, cylinderMaterial);
+		
+		var x = (i < 2) ? -1 : 1;
+		var z = (i % 2) ? -1 : 1;
+		var rotationAxis = new THREE.Vector3(x,0,z);
+		// makeRotationAxis wants its axis normalized
+		rotationAxis.normalize();
+		// don't use position, rotation, scale
+		cylinder.matrixAutoUpdate = false;
+		cylinder.matrix.makeRotationAxis( rotationAxis, theta );
+		scene.add( cylinder );
+	}
 }
 
 function drawHelpers() {
