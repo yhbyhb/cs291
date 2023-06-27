00be0eb4819fcfb2c844d8d9beb268c686a8389b
HanByul Yang <hanbyul.yang@gmail.com>
2023-06-27 16:52:32 +0900

    Head Light

--- unit6/08-head-light.js
+++ unit6/08-head-light.js
@@ -19,8 +19,8 @@ function fillScene() {
 	// LIGHTS
 	// Student: remove the ambient light, add a headlight;
 	// See render() for making the headlight match the camera position
-	scene.add( new THREE.AmbientLight( 0xFFFFFF ) );
-	// headlight = new ... point light
+	headlight = new THREE.PointLight( 0xFFFFFF, 1.0 );
+	scene.add( headlight );	
 	////////////////////
 
 
@@ -262,7 +262,7 @@ function render() {
 	cameraControls.update(delta);
 
 	// Student: set the headlight's position here.
-
+	headlight.position.copy( camera.position );
 	///////////////
 
 	renderer.render(scene, camera);
