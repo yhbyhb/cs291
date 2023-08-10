763e2318b0465be30586f315a41480c6d752e2ec
HanByul Yang <hanbyul.yang@gmail.com>
2023-08-10 14:47:24 +0900

    Shadow in three.js

--- unit6/14-shadows.js
+++ unit6/14-shadows.js
@@ -30,6 +30,7 @@ function init() {
 	renderer.gammaOutput = true;
 	renderer.setSize(canvasWidth, canvasHeight);
 	renderer.setClearColorHex( 0x0, 1.0 );
+	renderer.shadowMapEnabled = true;
 
 	// CAMERA
 	camera = new THREE.PerspectiveCamera( 35, canvasWidth/ canvasHeight, 1, 4000 );
@@ -55,6 +56,7 @@ function fillScene() {
 	spotlight.angle = 20 * Math.PI / 180;
 	spotlight.exponent = 1;
 	spotlight.target.position.set( 0, 200, 0 );
+	spotlight.castShadow = true;
 	scene.add( spotlight );
 
 	var lightSphere = new THREE.Mesh(
@@ -76,7 +78,7 @@ function fillScene() {
 			polygonOffset: true, polygonOffsetFactor: 1.0, polygonOffsetUnits: 4.0
 		}));
 	solidGround.rotation.x = -Math.PI / 2;
-
+	solidGround.receiveShadow = true;
 	scene.add( solidGround );
 
 	//////////////////////////////
